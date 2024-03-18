adaaf1ee3d75f3fe98b4cd5256cda05e0b0d530e
Renzo Espinosa B <renzoespinosa.b@gmail.com>
2024-03-18 18:48:46 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,52 @@
-import { useState } from 'react'
-import { DisplayAnecdotes } from './DisplayAnecdotes'
-import { DisplayWinner } from './DisplayWinner'
-import { DisplayAllAnecdotes } from './DisplayAllAnecdotes' // Por si quiero mostrar jugadores
+import { Course } from "./Course"
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [votes, setVotes] = useState({
-    0:0,
-    1:0,
-    2:0,
-    3:0,
-    4:0,
-    5:0,
-    6:0
-  })
-
-  const [selected, setSelected] = useState(-1)
-
-  const handleRandom =()=>{
-      setSelected(Math.floor(Math.random() * 7))
-  }
-
-  const handleVote = ()=>{
-    let el
-    const index = anecdotes.findIndex(el => el === anecdotes[selected])
-    el = index
-    const copy = {...votes}
-    copy[el] += 1 
-    setVotes(copy)
-  }
-
-  const findAnecdoteWinner = () => {
-    let maxValue = 0;
-    let anecdoteWithMostVotes = '';
-    
-    for (let id in votes) {
-      if (votes[id] > maxValue) {
-        maxValue = votes[id];
-        console.log(maxValue)
-        anecdoteWithMostVotes = anecdotes[id];
+    const course = [
+      {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
       }
-    }
-
-    return {winner: anecdoteWithMostVotes, votesForWinner: maxValue};
-  }
-
-  const {winner, votesForWinner} = findAnecdoteWinner();
+    ]
   
-    
-  return (
-    <>
-    <DisplayAnecdotes anecdotes={anecdotes} selected={selected}/>
-    <button onClick={handleRandom}>next anecdote</button>
-    <button onClick={handleVote}>vote</button> 
-    {/* <DisplayAllAnecdotes anecdotes={anecdotes} /> Si quiero mostrar ganadores */}
-    <DisplayWinner winnerId={winner} votesForWinner={votesForWinner}/>
-
-    </>
-  )
-}
-
-export default App
+    return <Course course={course} />
+  }
+  
+  export default App
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,24 +1,20 @@
+import React from 'react'
 
-export const data = {
-        name: 'Half Stack application development',
-        parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
+export const Course = ({course}) => {
+  const exercises = course.parts.map(x => x.exercises)
 
+    return (
+        <div>
+          <header><h1>{course.name}</h1></header>
+            {course.parts.map(part => 
+    
+              <p key={part.id}>
+                {part.name} {part.exercises}
+              </p>
+            )}
 
-
-
-
-  
+            <h4>total of {
+                exercises.reduce((acc,el)=> acc+el, 0)} exercises</h4> 
+        </div>
+      )
+}
