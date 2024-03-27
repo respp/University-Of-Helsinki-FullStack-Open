62d15337a6e4b7b75ad19a5ed38bd9b9c63089cf
Renzo Espinosa B <renzoespinosa.b@gmail.com>
2024-03-27 18:18:43 -0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from "react"
 import countryServices from "./services/countries"
+import { Notification } from "./Notification"
 
 const App = () => {
+  const [countries, setCountries] = useState([])
     const [filter, setFilter] = useState('')
+    const [message, setMessage] = useState(false)
+    const [filteredCountries, setFilteredCountries] = useState([])
 
     useEffect(()=>{
         countryServices
         .getAll()
-        .then(res => console.log(res.data))
+        .then(res =>{
+          const allNames = res.data.map(country => country.name.common)
+          setCountries(allNames)
+        })
     }, [])
 
+    
+
+    useEffect(() => {
+      const coincidences = countries.filter(country => country.toLowerCase().includes(filter.toLowerCase()))
+      if (coincidences.length > 10){
+        console.log(coincidences.length)
+        setMessage(`Too many matches, specify another filter`)
+      }
+      if(coincidences === 0){
+        setMessage(false)
+      }
+      setFilteredCountries(coincidences)
+    }, [countries, filter])
+    
+
+    const handleFilterChange = (e)=>{
+      const keyValue = e.target.value
+      setFilter(keyValue)
+    }
+
   return (
-<div>
-find countries 
-    <input
-        value={findCoutries}
-        onChange={handleFilter}
-    />
-</div>
+  <div>
+        find countries 
+        <input
+          value={filter}
+          onChange={handleFilterChange}
+        />
+        <Notification message={message}/>
+  </div>
   )
 }
 
