fcf976b1d1c3c7e91be4aaf65cd3bab1161dad64
respp <renzoespinosa.b@gmail.com>
2024-04-06 19:24:56 -0300

    updated style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { Filter } from './Filter'
 import { PersonForm } from './PersonForm'
 import { Persons } from './Persons'
+import { Notification } from './Notification'
+import { UpdateError } from './UpdateError'
 import personServices from './services/persons'
 
 const App = () => {
@@ -10,6 +12,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber ,setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(false)
+  const [updateError, setUpdateError] = useState(false)
 
   const hook = () => {
     personServices
@@ -32,7 +36,9 @@ const App = () => {
           const updatedNumber = {...person, number:newNumber}
           const id = person.id
 
-          personServices.update(id,updatedNumber).then(res=>{
+          personServices
+          .update(id,updatedNumber)
+          .then(res=>{
             console.log('se actualizó correctamente')
             setPersons(persons.map(person =>
               //Por cada persona si la id no es la que busco, la persona queda como antes, sino, cambia a la data que me de el servidor
@@ -40,6 +46,12 @@ const App = () => {
                     ? person
                     : res.data
             ))
+          })
+          .catch(err => {
+            setUpdateError(`Information of ${person.name} has already been removed from server (${err.message})`)
+            setTimeout(() =>{
+              setUpdateError(false)
+            },2500)
           })
         }
       }
@@ -53,25 +65,31 @@ const App = () => {
       .create(contactObject)
       .then(res =>{
         setPersons(persons.concat(res.data))
-        console.log(res.data)
-      }
-    )
+        setNotification(`Added ${res.data.name}`)
+        setTimeout(() =>{
+          setNotification(false)
+        },2500)
+        console.log('Added ',res.data)
+      })
+      .catch(res => console.log('error al crear', res))
       setNewName('')
       setNewNumber('')
+     }
     }
-  }
   return (
     <div>
       <h2>Phonebook</h2>
+        <UpdateError message={updateError}/>
         <Filter filter={filter} setFilter={setFilter}/>
           <form onSubmit={addContact}>
             <h2>add a new</h2>
             <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}/>
+            <Notification message={notification}/>
           </form>
         <h2>Numbers</h2>
           <Persons persons={persons} filter={filter} setPersons={setPersons}/>
     </div>
   )
-}
+  }
 
 export default App
