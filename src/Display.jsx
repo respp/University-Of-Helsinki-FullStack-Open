4faaef8df4967876f8e393ceef190d2c5e75b786
Micaela Dl <micadl@Air-de-Micaela.fibertel.com.ar>
2024-04-11 13:15:31 -0300

    made in micaelas house

---
+++ src/Display.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createLogger } from 'vite'
+
+export const Display = ({filteredCountries}) => {
+
+// console.log(filteredCountries.length , filteredCountries)
+if(filteredCountries.length == 0) return null
+
+// if (filteredCountries == 1){
+//   // console.log('succes', filteredCountries[0])
+//   return (
+//     <div>
+//       <h1>{filteredCountries[0].name.common}</h1>
+//     </div>
+//   )
+// }
+
+
+  return (
+    <div>
+      {filteredCountries.map(country => {
+        return(
+          <p key={country.length}>{country.name.common}</p>
+        )
+      })
+    }
+    </div>
+    
+  )
+}
