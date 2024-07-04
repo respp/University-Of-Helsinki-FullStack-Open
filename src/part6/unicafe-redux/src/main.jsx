6e1b692299cf819ebd4486c94ff24179649ddd79
Renzo Espinosa Barcos <renzoespinosa.b@gmail.com>
2024-07-04 11:48:36 -0300

    added unicafe

---
+++ src/part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+// import { configureStore } from '@reduxjs/toolkit'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+// const store = configureStore(reducer)
+
+const App = () => {
+
+console.log(store.getState())
+  return (
+    <div>
+      <button onClick={() => { store.dispatch({ type: 'GOOD' }) }}>good</button>  
+      <button onClick={() => { store.dispatch({ type: 'OK' }) }}>ok</button> 
+      <button onClick={() => { store.dispatch({ type: 'BAD' }) }}>bad</button>
+      <button onClick={() => { store.dispatch({ type: 'ZERO' }) }}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
