68cc711738036664b8ef922da483b403f4354dc4
Renzo Espinosa Barcos <renzoespinosa.b@gmail.com>
2024-07-26 01:08:49 -0300

    up

--- src/part7/routed-anecdotes/src/components/About.jsx
+++ src/part7/routed-anecdotes/src/components/About.jsx
@@ -2,7 +2,7 @@
 export const About = () => (
     <div>
       <h2>About anecdote app</h2>
-      <p>According to Wikipedia:</p>
+      <p>According to Wikipedia: </p>
   
       <em>An anecdote is a brief, revealing account of an individual person or an incident.
         Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
