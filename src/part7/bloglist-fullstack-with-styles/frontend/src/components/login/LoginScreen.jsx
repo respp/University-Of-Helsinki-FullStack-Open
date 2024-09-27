5338e77fdbe745b4bef3259361376c504faf9aad
Micaela Dl <micadl@MacBook-Air-de-Micaela.local>
2024-09-27 14:51:51 -0400

    new rama

--- src/part7/bloglist-fullstack-with-styles/frontend/src/components/login/LoginScreen.jsx
+++ src/part7/bloglist-fullstack-with-styles/frontend/src/components/login/LoginScreen.jsx
@@ -6,15 +6,22 @@ import { LoginForm } from "./LoginForm";
 export const LoginScreen = () => {
 
   return (
-    <div className="log">    
-    <div className="login-screen">
+    <div className="log">  
+    
+      {/* flor*/}
+    <div className="login-screen"> 
+
+      {/*conjunto de textos*/}
       <div className="title-subtitle">
         <h2>BlogNest</h2>
         <h6>BlogNest facilita la gestión y el intercambio de ideas y experiencias en línea.</h6>
       </div>
+
+      {/* formulario*/}
       <div className="form">
           <LoginForm />
       </div>
+
     </div>
     <Error />
     </div>
